350db9294225c38f573c4ede00eab6dd4ae18a2a
Maksfin <dyrnoj@bk.ru>
2015-11-23 02:27:23 +0300

    finished typeset and start media-queries

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,44 @@
+var mainModule = (function() {
+	// приватные св-ва и методы
+
+	// прослушка событий
+	var _setUpListners = function() {
+		$('.nav-menu__item').on('click', _menuDrop)
+	},
+		_menuDrop = function(e) {
+			$this = $(this),
+			container = $this.closest('.nav-menu__list'),
+			items = container.find('.nav-menu__item'),
+			drops = $('.nav-menu-drop__list'),
+			back = drops.find('.nav-menu-drop__item:first');
+
+			items.fadeOut(function() {
+				drops.fadeIn();
+
+			});
+
+			back.on('click', function() {
+				drops.fadeOut(function() {
+					items.fadeIn();
+
+			});
+		});
+	};
+
+	
+	// публичные св-ва и методы
+	return {
+		init : function() {
+			_setUpListners();
+		}
+	};
+})();
+
 $(document).ready(function($) {
 
- if ($('.form__select-item').length) {
- 	$('.form__select-item').select2({
- 		minimumResultsForSearch: Infinity
- 	});
- }
+ if ($('.nav-menu__list').length) {
+ 		mainModule.init();
+ 	}
 
 }); // --> end
  
